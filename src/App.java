0035de36fd392fe9342bf92afa1ac752bab40595
mtinhinane <tinhinane.messaoudene@etu.u-pec.fr>
2018-02-13 12:55:07 +0100

    add application

---
+++ src/App.java
@@ -0,0 +1,16 @@
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+
+/**
+ * GUI CRUD with Pool connection
+ * @author Suriya, Tinhinane
+ */
+public class App {
+
+	public static void main(String[] args) throws SQLException {	
+		CrudGUI c = new CrudGUI ();
+		//CRUD.delete(2);
+	}
+}
